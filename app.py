d68bf26224417014e0b5d4f98fdc41f10cd41e77
“arnaupc” <arnaupc@hotmail.com>
2023-08-03 21:28:41 +0200

    folder structure

--- app.py
+++ app.py
@@ -7,42 +7,30 @@ import click
 # Create Flask application
 def create_app():
 
-    app = Flask(__name__, instance_relative_config=False)  # set app
-    app.config.from_pyfile('./config.py')  # load config
+    flask_app = Flask(__name__, instance_relative_config=False)  # set app
+    flask_app.config.from_pyfile('./config.py')  # load config
 
     # Init assets
     assets = Environment()
-    assets.init_app(app)
+    assets.init_app(flask_app)
 
-    with app.app_context():
+    with flask_app.app_context():
         # Import parts of our application
         
         # Frontend register
-        from frontend.views import bp as front
-        app.register_blueprint(front)
+        from apps.frontend.views import bp as front
+        flask_app.register_blueprint(front)
 
         # API register
-        from api.views import bp as api
-        app.register_blueprint(api)
+        from apps.api.views import bp as api
+        flask_app.register_blueprint(api)
 
         # Import commands
-        from commands.utils import bp as cmd_utils
-        app.register_blueprint(cmd_utils)
+        from apps.commands.utils import bp as cmd_utils
+        flask_app.register_blueprint(cmd_utils)
 
         # Compile static assets
-        from assets import compile_static_assets
+        from apps.assets import compile_static_assets
         compile_static_assets(assets)
 
-
-        # @app.cli.command()
-        # @click.command('hello')
-        # @click.option('--count', default=1, help='Number of greetings.')
-        # @click.option('--name', prompt='Your name', help='The person to greet.')
-        # @with_appcontext
-        # def hello_cmd(count, name):
-        #     print(count, name)
-        #     # Simple program that greets NAME for a total of COUNT times.
-        #     for _ in range(count):
-        #         click.echo(f'Hello, {name}!')
-
-        return app
+        return flask_app
